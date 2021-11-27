7ec1fea4a2f697f472a525410f2d5f1ad426fc31
Sushant Sahu <sushant87@gmail.com>
2021-11-27 20:49:45 +0530

    wip: implemented http verbs

--- 09-webserver/main.go
+++ 09-webserver/main.go
@@ -12,25 +12,53 @@ import (
 
 func getPostsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{
-		"message": "hello world from GET POSTS"},
-	)
+	json.NewEncoder(w).Encode(posts)
 }
 
 func getPostHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{
-		"message": "hello world from GET POST"},
-	)
+
+	params := mux.Vars(r)
+	var post Post
+
+	for _, p := range posts {
+		if p.ID == params["id"] {
+			post = p
+		}
+	}
+
+	json.NewEncoder(w).Encode(post)
 }
 
+// TODO
 func updatePostHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{
-		"message": "hello world from PUT POST"},
-	)
+
+	var post Post
+	var updatedPost Post
+
+	err := json.NewDecoder(r.Body).Decode(&updatedPost)
+	for _, p := range posts {
+		if p.ID == updatedPost.ID {
+			post = p
+			break
+		}
+	}
+
+	post.Title = updatedPost.Title
+	post.Author = updatedPost.Author
+
+	if err != nil {
+		json.NewEncoder(w).Encode(map[string]string{
+			"message": "an error has occurred"},
+		)
+	}
+
+	json.NewEncoder(w).Encode(posts)
+
 }
 
+// TODO
 func deletePostHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{
@@ -41,9 +69,40 @@ func deletePostHandler(w http.ResponseWriter, r *http.Request) {
 
 func addPostHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{
-		"message": "hello world from POST POST"},
-	)
+
+	var post Post
+	json.NewDecoder(r.Body).Decode(&post)
+	posts = append(posts, post)
+	json.NewEncoder(w).Encode(posts)
+}
+
+type Post struct {
+	ID     string `json:"id,omitempty"`
+	Title  string `json:"title,omitempty"`
+	Author string `json:"author,omitempty"`
+}
+
+var posts = []Post{
+	{
+		ID:     "1",
+		Title:  "Learning Go",
+		Author: "Sushant Sahu",
+	},
+	{
+		ID:     "2",
+		Title:  "Fashion By Day",
+		Author: "Khushboo Sewak",
+	},
+	{
+		ID:     "3",
+		Title:  "Fashion By Night",
+		Author: "Khushboo Sewak",
+	},
+	{
+		ID:     "4",
+		Title:  "Learning Spark",
+		Author: "Sushant Sahu",
+	},
 }
 
 func main() {
